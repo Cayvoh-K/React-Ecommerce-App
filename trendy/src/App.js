e6fb15adace663479e23d73e51e189f46f339466
moses <cayvoh@mosess-MacBook-Pro.local>
2023-06-26 09:15:15 +0300

    new correction

--- trendy/src/App.js
+++ trendy/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import Home from './pages/Home';
@@ -10,13 +10,13 @@ function App() {
     return (
         <div className="App">
             <Provider store={store}>
-                <HashRouter>
+                <BrowserRouter basename="/React-Ecommerce-App" >
                     <Navbar />
                     <Routes>
                         <Route path="/" element={<Home />}></Route>
                         <Route path="/cart" element={<Cart />}></Route>
                     </Routes>
-                </HashRouter>
+                </BrowserRouter>
             </Provider>
         </div>
     );
